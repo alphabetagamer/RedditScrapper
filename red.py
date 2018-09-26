00ccd96f457989701f01ad0c3d388e662a085344
alphabeta_g <34796341+alphabetagamer@users.noreply.github.com>
2018-09-26 22:39:25 +0530

    Add files via upload
    The files are stored with a random name in the directory the script is run. It's a python script , you might need to download the required libraries

---
+++ red.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 11:29:43 2018
+
+@author: lokin
+"""
+
+import praw
+import random
+import urllib.request
+
+reddit = praw.Reddit(client_id='9wK2B_VK2ubjbg',
+                     client_secret='0tZu4owtCPURqp4RJTY3yckEr88',
+                     password='24663915',
+                     user_agent='Data Scraping',
+                     username='notabota123')
+#for submission in reddit.subreddit('gentlemanboners').hot(limit=20) :
+for submission in reddit.redditor('dendritesofdendrites').submissions.top('all') :
+    if not submission.stickied:
+        img_name = random.randrange(0, 8989) 
+        a=submission.url.split('.')
+       
+        if a[0]!='https://gfycat':
+            if submission.url[-4:]!='.png' and submission.url[-4:]!='.jpg':
+                submission.url=submission.url+'.png'
+            else:
+                pass
+            print(submission.url)
+            full_name = str(img_name) + '.png'
+            urllib.request.urlretrieve(submission.url,full_name)
+
+      
+
